995fbe1e4653a415d7f7884629d8ac48f0116d2b
Andrea Ranzato <andrea.ranzato01@gmail.com>
2025-10-07 23:27:15 +0200

    chore: remove header's helper functions

--- R/cbi.R
+++ R/cbi.R
@@ -17,31 +17,6 @@ is_header <- function(x) {
   grepl(pattern_header(), x)
 }
 
-header_code <- function(x) {
-  assert_header_code(x)
-  substr(x, 1L, 2L)
-}
-
-bank_code <- function(x) {
-  assert_header_code(x)
-  substr(x, 3L, 7L)
-}
-
-company_code <- function(x) {
-  assert_header_code(x)
-  substr(x, 8L, 12L)
-}
-
-date <- function(x) {
-  assert_header_code(x)
-  substr(x, 13L, 18L)
-}
-
-account_number <- function(x) {
-  assert_header_code(x)
-  substr(x, 19, nchar(x))
-}
-
 # Summary record ----
 
 is_summary <- function(x) {
--- tests/testthat/test-cbi.R
+++ tests/testthat/test-cbi.R
@@ -3,29 +3,6 @@ test_that("is_header works", {
   expect_true(is_header(x))
 })
 
-test_that("header_code works", {
-  expect_equal(header_code("RH08982AEN6D080825INBK5389029800100606"), "RH")
-})
-
-test_that("bank_code works", {
-  expect_equal(bank_code("RH08982AEN6D080825INBK5389029800100606"), "08982")
-})
-
-test_that("company_code works", {
-  expect_equal(company_code("RH08982AEN6D080825INBK5389029800100606"), "AEN6D")
-})
-
-test_that("date works", {
-  expect_equal(date("RH08982AEN6D080825INBK5389029800100606"), "080825")
-})
-
-test_that("account_number works", {
-  expect_equal(
-    account_number("RH08982AEN6D080825INBK5389029800100606"),
-    "INBK5389029800100606"
-  )
-})
-
 test_that("is_closing works", {
   expect_true(is_closing(
     "640000001EUR080825C000000014642,37C000000014642,37"
